314c2bdec8e9b26c8f40ea4617a2251568747924
Bofino <0X45256973@gmail.com>
2018-04-23 20:31:24 +0200

    was added description

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -5,8 +5,8 @@ import gulp from 'gulp';
 
 import clean from './booster/taskClean';     // Remove destination folder
 import fonts from './booster/taskFonts';     // Unzip necessary fonts
-import style from './booster/taskStyle';   // Creates a font-face stylesheet for selected fonts
+import style from './booster/taskStyle';     // Creates a font-face stylesheet for selected fonts
 
 let webfonts = gulp.series(clean, gulp.parallel(fonts, style));
 
-export default webfonts;
+export default webfonts;                     // Now you can run just <gulp> in the command line
