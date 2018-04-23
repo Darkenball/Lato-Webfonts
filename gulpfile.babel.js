ff16d1ae589c4f3301858fcef9a16040081b03cd
Bofino <0X45256973@gmail.com>
2018-04-23 19:24:38 +0200

    added .babelrc .editorconfig .gitattributes .gitignore gulpfile.babel.js packege-lock.json package.json to master

---
+++ gulpfile.babel.js
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------------------------------------------------
+// ------ Gulp file
+// ----------------------------------------------------------------------------------------------------
+import gulp from 'gulp';
+
+import clean from './booster/taskClean';     // Remove destination folder
+import fonts from './booster/taskFonts';     // Unzip necessary fonts
+import style from './booster/taskStyle';   // Creates a font-face stylesheet for selected fonts
+
+let webfonts = gulp.series(clean, gulp.parallel(fonts, style));
+
+export default webfonts;
