245622a7036920f9539eebdc6605c2c631a666bf
Bofino <0X45256973@gmail.com>
2018-04-23 21:22:44 +0200

    added gulp-sourcemaps to create fonts.css.map

--- booster/taskStyle.js
+++ booster/taskStyle.js
@@ -3,13 +3,16 @@
 // ----------------------------------------------------------------------------------------------------
 import gulp from 'gulp';
 import sass from 'gulp-sass';
+import sourcemaps from 'gulp-sourcemaps';
 
 class Style {
 
   static createCss() {
 
    return gulp.src('source/scss/fonts.scss')
+            .pipe(sourcemaps.init())
             .pipe(sass())
+            .pipe(sourcemaps.write('./maps', {includeContent: false}))
             .pipe(gulp.dest('dist/'));
   }
 }
